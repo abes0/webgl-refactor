1cc67fef9a064e7f84bf4e564eade8cad335db6a
Shinya Abe <s-abe@details.co.jp>
2022-08-17 17:51:45 +0900

    add easy Routing

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import "./style.scss";
 import CExample from "./component/cExample";
+import CRefractTest from "./component/cRefractTest";
 
 window.addEventListener("DOMContentLoaded", () => {
-  new CExample();
+  const path = location.pathname;
+  switch (path) {
+    case "/refract/":
+      console.log(path);
+      new CRefractTest();
+      break;
+    default:
+      new CExample();
+  }
 });
