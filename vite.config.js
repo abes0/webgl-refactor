f5bd2175c7f2e194de62adea88c67c671d2899f9
Shinya Abe <s-abe@details.co.jp>
2022-08-19 10:32:43 +0900

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import fs from "fs";
 
 export default defineConfig({
   assetsInclude: ["**/*.gltf", "**/*.bin"],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       // entry chunk assets それぞれの書き出し名の指定
-  //       // entryFileNames: `assets/[name].js`,
-  //       chunkFileNames: `[name].js`,
-  //       assetFileNames: `assets/[name].[ext]`,
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      //     output: {
+      //       // entry chunk assets それぞれの書き出し名の指定
+      //       // entryFileNames: `assets/[name].js`,
+      //       chunkFileNames: `[name].js`,
+      //       assetFileNames: `assets/[name].[ext]`,
+      //     },
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "slider/index.html"),
+      },
+    },
+  },
 });
